e385c1de533915f6b5c3672d007442509438c10d
Pavel Mirovshchikov <pmirov@gmail.com>
2025-02-07 22:45:59 +0700

    добавил вывод файлов

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,7 +1,53 @@
 package Task1;
 
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь к первому файлу:");
+        String firstFile = scanner.next();
+        File file1 = new File(firstFile);
+        System.out.println("Введите путь ко второму файлу:");
+        String secondFile = scanner.next();
+        File file2 = new File(secondFile);
+
+        filePrint(file1);
+        filePrint(file2);
+
+
     }
-}
+
+    public static void filePrint(File file)
+    {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            List<String> lines = new ArrayList<String>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            String [] linesAsArray = lines.toArray(new String[0]);
+            reader.close();
+            for (String item : linesAsArray)
+            {
+                System.out.println(item);
+            }
+
+        } catch (IOException e) {
+            System.out.println("ERROR");
+        };
+    }
+
+
+
+
+    }
+
