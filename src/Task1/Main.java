3da7c4c01f8d213475eb71974583d0304785754f
Pavel Mirovshchikov <pmirov@gmail.com>
2025-02-07 23:29:49 +0700

    добавил функцию вывода и сравнения файлов

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -3,7 +3,6 @@ package Task1;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,31 +18,70 @@ public class Main {
         String secondFile = scanner.next();
         File file2 = new File(secondFile);
 
-        filePrint(file1);
-        filePrint(file2);
+        //filePrint(file1);
+        //filePrint(file2);
+        compareFileStrings(file1,file2);
 
 
     }
 
-    public static void filePrint(File file)
+    public static String[] filePrint(File file)
     {
+        String [] linesAsArray = null;
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
-            List<String> lines = new ArrayList<String>();
+            List<String> lines = new ArrayList<>();
             String line;
             while ((line = reader.readLine()) != null) {
                 lines.add(line);
             }
-            String [] linesAsArray = lines.toArray(new String[0]);
+            linesAsArray = lines.toArray(new String[0]);
             reader.close();
-            for (String item : linesAsArray)
-            {
-                System.out.println(item);
-            }
-
-        } catch (IOException e) {
+//            for (String item : linesAsArray)
+//            {
+//                System.out.println(item);
+//            }
+            return linesAsArray;
+        }
+        catch (IOException e) {
             System.out.println("ERROR");
         };
+        return linesAsArray;
+    }
+
+    public static void compareFileStrings(File file1, File file2)
+    {
+
+
+            String [] lineArray1 = filePrint(file1);
+            String [] lineArray2 = filePrint(file2);
+            int len1 = lineArray1.length;
+            int len2 = lineArray2.length;
+            String [] allLines = new String[len1 + len2];
+            System.arraycopy(lineArray1,0,allLines,0,lineArray1.length);
+            System.arraycopy(lineArray2, 0, allLines, lineArray1.length, lineArray2.length);
+            List<String> duplicates = new ArrayList<>();
+        for (int i = 0; i < allLines.length; i++) {
+            String item1 = allLines[i];
+            if (item1 == null) continue;
+            for (int j = 0; j < allLines.length; j++) {
+                if( i == j) continue;
+                String item2 = allLines[j];
+                if (item1.equals((item2)))
+                {
+                    duplicates.add(item2);
+                }
+            }
+
+
+        }
+
+        for (String item : duplicates)
+        {
+            System.out.println(item);
+        }
+
+
     }
 
 
