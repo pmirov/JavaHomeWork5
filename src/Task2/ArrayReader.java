c31e1189b924986e46a43f4d46e514326e6b39cf
Pavel Mirovshchikov <pmirov@gmail.com>
2025-02-10 23:10:59 +0700

    сделал реализацию для вывода массисив, создал дополнительный класс для чтения и пребразования в массив

---
+++ src/Task2/ArrayReader.java
@@ -0,0 +1,9 @@
+package Task2;
+
+public class ArrayReader {
+    private String filePath;
+
+    public ArrayReader(String filePath) {
+        this.filePath = filePath;
+    }
+}
--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -1,7 +1,42 @@
 package Task2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/Task2/file.txt"));
+        List <String> stringArrays = new ArrayList<>();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            stringArrays.add(line);
+        }
+
+        String[] firstArrayStr = stringArrays.getFirst().split("\\s");
+        int[] firstArray = new int[firstArrayStr.length];
+        for(int i = 0;i < firstArrayStr.length;i++)
+        {
+            firstArray[i] = Integer.parseInt(firstArrayStr[i]);
+        }
+        String[] secondArray = stringArrays.getLast().split("\\s");
+
+        System.out.println("Массив 1: ");
+
+        for (int i = 0; i < firstArray.length; i++) {
+            System.out.print(firstArray[i] + " ");
+
+        }
+        System.out.println("\nМассив 2: ");
+        for (int i = 0; i < secondArray.length; i++) {
+            System.out.print(secondArray[i] + " ");
+
+        }
+
 
     }
 }
