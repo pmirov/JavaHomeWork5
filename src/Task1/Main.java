6eb1f57f70bfc3b31d04f144b3b229910d2632d9
Pavel Mirovshchikov <pmirov@gmail.com>
2025-02-10 22:12:05 +0700

    исправил метод вывода неповторяющихся строк

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -2,10 +2,7 @@ package Task1;
 
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
+import java.util.*;
 
 
 public class Main {
@@ -18,67 +15,42 @@ public class Main {
         String secondFile = scanner.next();
         File file2 = new File(secondFile);
 
-        //filePrint(file1);
-        //filePrint(file2);
         compareFileStrings(file1,file2);
 
 
     }
 
-    public static String[] filePrint(File file)
+    public static List<String> filePrint(File file)
     {
-        String [] linesAsArray = null;
+        List <String> linesAsList = null;
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
-            List<String> lines = new ArrayList<>();
+            linesAsList = new ArrayList<>();
             String line;
             while ((line = reader.readLine()) != null) {
-                lines.add(line);
+                linesAsList.add(line);
             }
-            linesAsArray = lines.toArray(new String[0]);
+
             reader.close();
-//            for (String item : linesAsArray)
-//            {
-//                System.out.println(item);
-//            }
-            return linesAsArray;
+            return linesAsList;
         }
         catch (IOException e) {
             System.out.println("ERROR");
         };
-        return linesAsArray;
+        return linesAsList;
     }
 
     public static void compareFileStrings(File file1, File file2)
     {
 
 
-            String [] lineArray1 = filePrint(file1);
-            String [] lineArray2 = filePrint(file2);
-            int len1 = lineArray1.length;
-            int len2 = lineArray2.length;
-            String [] allLines = new String[len1 + len2];
-            System.arraycopy(lineArray1,0,allLines,0,lineArray1.length);
-            System.arraycopy(lineArray2, 0, allLines, lineArray1.length, lineArray2.length);
-            List<String> duplicates = new ArrayList<>();
-        for (int i = 0; i < allLines.length; i++) {
-            String item1 = allLines[i];
-            if (item1 == null) continue;
-            for (int j = 0; j < allLines.length; j++) {
-                if( i == j) continue;
-                String item2 = allLines[j];
-                if (item1.equals((item2)))
-                {
-                    duplicates.add(item2);
-                }
-            }
-
-
-        }
-
-        for (String item : duplicates)
-        {
-            System.out.println(item);
+            List<String> lineList1 = filePrint(file1);
+            List<String> lineList2 = filePrint(file2);
+            List<String> allLines = new ArrayList<>(lineList1);
+            allLines.removeAll(lineList2);
+        System.out.println("Несовпадающие строки из каждого файла:");
+        for (String line : allLines) {
+            System.out.println(line);
         }
 
 
