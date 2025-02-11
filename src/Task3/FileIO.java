a88cb32e50af4e072ceb8d75079bc8de1e8df68e
Pavel Mirovshchikov <pmirov@gmail.com>
2025-02-11 23:43:04 +0700

    добавил класс записи в файл и реализовал запись согл. заданию

--- src/Task3/FileIO.java
+++ src/Task3/FileIO.java
@@ -1,5 +1,18 @@
 package Task3;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class FileIO {
-    
+    public static void writeIntToFile(String filePath, int[] array) throws IOException {
+        File file = new File(filePath);
+        FileOutputStream outputWriter = new FileOutputStream(file, true);
+
+        for (int i = 0; i < array.length; i++) {
+            outputWriter.write((array[i] + " ").getBytes());
+        }
+        outputWriter.write(("\n").getBytes());
+        outputWriter.close();
+    }
 }
--- src/Task3/Main.java
+++ src/Task3/Main.java
@@ -10,29 +10,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         //System.out.println("Введите путь к первому файлу:");
-        //String filePath = scanner.next();
-        File file = new File("src/Task3/file.txt");
+        String filePath = "src/Task3/file.txt";
+
+        File file = new File(filePath);
         System.out.println("Введите массив целых чисел:");
         String data = scanner.nextLine();
         String[] arrayStr = data.split("\\s");
         int[] array = new int[arrayStr.length];
-        for (int i = 0; i < arrayStr.length; i++) {
+        for (int i = 0; i < arrayStr.length; i++)
+        {
             array[i] = Integer.parseInt(arrayStr[i]);
         }
-        FileOutputStream outputWriter = new FileOutputStream(file);
-//        for(int i=0; i < array.length; i++)
-//        {
-//            outputWriter.write(array[i]);
-//        }
-        outputWriter.write(data.getBytes());
-         int[] evenArray = ArrayModifier.evenNumbers(array,array.length);
+        FileOutputStream clearFile = new FileOutputStream(filePath);
+        clearFile.close();
+        FileIO.writeIntToFile(filePath,array);
+
+        int[] evenArray = ArrayModifier.evenNumbers(array,array.length);
          int[] oddArray = ArrayModifier.oddNumbers(array,array.length);
          int[] reverseArray = ArrayModifier.reversArray(array,array.length);
 
-        for (int i : reverseArray) {
-            System.out.println(i);
-        }
+        FileIO.writeIntToFile(filePath,evenArray);
+        FileIO.writeIntToFile(filePath,oddArray);
+        FileIO.writeIntToFile(filePath,reverseArray);
 
-         
+
+
+
     }
 }
